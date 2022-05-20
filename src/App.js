7402f1c5e49136d958d11511a5db9fae3d404123
Louis Ruocco <louisruocco1@gmail.com>
2022-05-20 17:47:18 +0100

    search function working

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ const App = () => {
 
   const [movies, setMovies] = useState([]);
   const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("Inception");
 
-  const url = `https://imdb-api.com/en/API/SearchMovie/${process.env.REACT_APP_KEY}/The Dark Knight`
+  const url = `https://imdb-api.com/en/API/SearchMovie/${process.env.REACT_APP_KEY}/${query}`
 
   const getFilms = async () => {
     const response = await fetch(url);
@@ -18,23 +19,27 @@ const App = () => {
 
   const updateSearch = e => {
     setSearch(e.target.value);
-    console.log(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search)
   }
 
   useEffect(() => {
     getFilms();
-  }, [])
+  }, [query])
 
   return (
     <div className = "App">
       <h1>Movie Lookup</h1>
-      <form>
+      <form onSubmit={getSearch} className="search-form">
         <input type="text" className="search" value={search} placeholder="Search for a Movie..." onChange={updateSearch}/>
         <button className="search-btn">Search</button>
       </form>
       {movies.map(movie => (
          <Movie
-            key={movie.title}
+            key={movie.id}
             title={movie.title}
             description={movie.description}
             image={movie.image}
